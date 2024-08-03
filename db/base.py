32f230a8b9cfda1cf5002c0e6a00474cb8b6b37c
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-03 22:12:18 +0300

    Remove unused import

--- db/base.py
+++ db/base.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine
-from dotenv import load_dotenv
 from core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
