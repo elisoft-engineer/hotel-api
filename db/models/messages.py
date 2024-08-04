7d861dcafbd44697ed45d8bf03dbe338cfd6d3d2
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-04 11:17:19 +0300

    Initial Commit

---
+++ db/models/messages.py
@@ -0,0 +1,15 @@
+from ..base import Base
+from .mixins import Timestamp
+from sqlalchemy import Column, UUID, String, Text
+from uuid import uuid4
+
+class Message(Base, Timestamp):
+    __tablename__ = "messages"
+
+    id = Column(UUID, default=uuid4, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    detail = Column(Text)
+
+    def __repr__(self):
+        return self.email
