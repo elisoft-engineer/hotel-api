c3fdc78f06438cfc50f85aaaeb98952a0fd20f38
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-04 11:18:00 +0300

    Add docstrings

--- db/models/mixins.py
+++ db/models/mixins.py
@@ -3,9 +3,16 @@ from datetime import datetime, timezone
 from sqlalchemy import Column, DateTime
 from sqlalchemy.sql import func
 
+"""
+Almost all the database models require the timestamps fields. It's better to
+have the Timestamp class and then have it subclassed by all the models for
+better code.
+"""
+
 @declarative_mixin
 class Timestamp:
     __abstract__ = True
 
-    created_at = Column(DateTime(timezone=True), default=func.now(), nullable=False)
-    modified_at = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    modified_at = Column(DateTime(timezone=True), default=func.now())
+    
