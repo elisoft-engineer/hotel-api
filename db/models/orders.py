e340132ac3a3337cff3bdbf6120a45d67d70679d
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-04 11:18:37 +0300

    Initial Commit

---
+++ db/models/orders.py
@@ -0,0 +1,18 @@
+from ..base import Base
+from .mixins import Timestamp
+from sqlalchemy import Column, UUID, DECIMAL, ForeignKey, Enum
+from uuid import uuid4
+from .enums import OrderStatus
+from sqlalchemy.orm import relationship
+from .assotiations import order_menu_association
+
+class Order(Base, Timestamp):
+    __tablename__ = "orders"
+
+    id = Column(UUID, default=uuid4, primary_key=True)
+    amount = Column(DECIMAL)
+    customer_id = Column(ForeignKey("customers.id"))
+    status = Column(Enum(OrderStatus, name="order_status"))
+
+    customer = relationship("Customer", back_populates="orders")
+    items = relationship("Menu", secondary=order_menu_association, back_populates="orders")
