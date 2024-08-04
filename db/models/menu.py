a26ef3fe72b74c7f2506e1954e92f5ba91191cb4
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-04 11:17:10 +0300

    Add the review relationship

--- db/models/menu.py
+++ db/models/menu.py
@@ -1,31 +1,40 @@
 from sqlalchemy import Column, UUID, String, Text, DECIMAL, Enum, Integer, ForeignKey
-from .category import MenuCategory
+from .enums import MenuCategory
 from uuid import uuid4
 from ..base import Base
 from .mixins import Timestamp
 from sqlalchemy.orm import relationship
 
+
 class Menu(Base, Timestamp):
     __tablename__ = "menu"
 
     id = Column(UUID, default=uuid4, primary_key=True)
-    name = Column(String, nullable=False, index=True)
-    description = Column(Text(), nullable=False)
-    price = Column(DECIMAL, nullable=False)
+    name = Column(String, index=True)
+    description = Column(Text())
+    price = Column(DECIMAL)
     category = Column(Enum(MenuCategory, name="menu_category"))
     image = Column(String, nullable=True)
     thumbnail = Column(String, nullable=True)
 
+    reviews = relationship("Review", back_populates="item")
+
+    def __repr__(self):
+        return f"{self.__class__.name}: {self.name}"
     
 
 class Review(Base, Timestamp):
     __tablename__ = "reviews"
 
     id = Column(UUID, default=uuid4, primary_key=True)
-    message = Column(Text(), nullable=False)
-    rating = Column(Integer, nullable=False)
+    message = Column(Text())
+    rating = Column(Integer)
 
     menu_id = Column(ForeignKey("menu.id"))
     customer_id = Column(ForeignKey("customer.id"))
 
     menu = relationship("Menu", back_populates="reviews")
+    customer = relationship("Customer", back_populates="reviews")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}: {str(self.message)[:15]}... {self.rating}"
