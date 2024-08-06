234fc953813853b23bad99ce7ba5adb880a9dd33
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-06 13:02:06 +0300

    Initial notifications CRUD utils

---
+++ db/crud/notifications.py
@@ -0,0 +1,73 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from uuid import UUID
+from db.models import notifications as models, enums
+from db.schemas import notifications as schemas
+
+
+async def get_user_notifications(
+    db: AsyncSession,
+    user_id: UUID,
+    offset: int | None = None,
+    limit: int | None = None
+):
+    results = await db.execute(select(models.Notification).where(models.Notification.user_id == user_id).offset(offset).limit(limit))
+    return results.scalars().all() # Look up how to order the status
+
+
+async def get_user_unread_notifications(
+    db: AsyncSession,
+    user_id: UUID,
+    offset: int | None = None,
+    limit: int | None = None
+):
+    results = await db.execute(select(models.Notification).where(models.Notification.user_id == user_id and models.Notification.status == enums.NotificationStatus.UNREAD).offset(offset).limit(limit))
+    return results.scalars().all()
+
+
+async def get_user_read_notifications(
+    db: AsyncSession,
+    user_id: UUID,
+    offset: int | None = None,
+    limit: int | None = None
+):
+    results = await db.execute(select(models.Notification).where(models.Notification.user_id == user_id and models.Notification.status == enums.NotificationStatus.READ).offset(offset).limit(limit))
+    return results.scalars().all()
+
+
+async def get_notification(db: AsyncSession, id: UUID):
+    result = await db.execute(select(models.Notification).where(models.Notification.id == id))
+    return result.scalars().first()
+
+
+async def create_notification(db: AsyncSession, notification: schemas.NotificationCreate):
+    notification_data = notification.model_dump()
+    db_notification = models.Notification(**notification_data)
+    db.add(db_notification)
+    await db.commit()
+    await db.refresh(db_notification)
+    return db_notification
+
+
+async def mark_notification_as_read(db: AsyncSession, id: UUID):
+    notification = await get_notification(db, id)
+    if not notification:
+        return None
+    
+    setattr(notification, "status", enums.NotificationStatus.READ)
+    db.add(notification)
+    await db.commit()
+    await db.refresh(notification)
+    return {"message": "Notification read successfully"}
+
+
+async def mark_all_notifications_as_read(db: AsyncSession, user_id: UUID):
+    unread_notifications = get_user_unread_notifications(db, user_id)
+
+    for notification in unread_notifications:
+        setattr(notification, "status", enums.NotificationStatus.READ)
+        db.add(notification)
+        await db.commit()
+        await db.refresh(notification)
+
+    return {"message": "Notifications read successfully"}
