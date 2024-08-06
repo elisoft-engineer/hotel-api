156089361f2e0cd64f1e05d90d168a0935dcd9e2
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-06 11:28:29 +0300

    Add the notification status enum

--- db/models/enums.py
+++ db/models/enums.py
@@ -5,14 +5,19 @@ The menu category enum serves a better implementation and easier crud utilities
 for menu items.
 """
 
-class MenuCategory(Enum):
+class MenuCategory(str, Enum):
     MAIN_COURSES = "main_courses"
     DRINKS = "drinks"
+    OTHER = "other"
     # I shall add the enum types later
     
 
-class OrderStatus(Enum):
-    PENDING = "Pending"
-    DELIVERED = "Delivered"
-    PAID = "Paid"
-    CANCELLED = "Cancelled"
+class OrderStatus(str, Enum):
+    PENDING = "pending"
+    COMPLETED = "completed"
+    CANCELLED = "cancelled"
+
+
+class NotificationStatus(str, Enum):
+    READ = "read"
+    UNREAD = "unread"
