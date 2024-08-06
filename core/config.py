9c89ecbdef5b1bd5cee724d10becef7d27dd8099
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-06 13:34:48 +0300

    Handle the files dirs

--- core/config.py
+++ core/config.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
-import os
+from os import getenv, path, makedirs
 
+load_dotenv()
 
 """
 This file handles the app configuration such as the database uri, file system
@@ -8,7 +9,17 @@ management, e.t.c
 """
 
 class Settings:
-    DATABASE_URL: str = os.getenv('DATABASE_URL')
+    SECRET_KEY: str = getenv("SECRET_KEY")
+    ALGORITHM: str = getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    DATABASE_URL: str = getenv('DATABASE_URL')
+    
+    BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+    FILES_DIR = path.join(BASE_DIR, "files")
+    MENU_DIR = path.join(FILES_DIR, "menu")
+
+    makedirs(FILES_DIR, exist_ok=True)
+    makedirs(MENU_DIR, exist_ok=True)
 
 
 settings = Settings()
