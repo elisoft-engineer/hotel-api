936c2a302db2ece9fe70c6490442ca3db2538d74
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-06 11:30:58 +0300

    Add the notification status column to the model

--- db/models/notifications.py
+++ db/models/notifications.py
@@ -1,6 +1,7 @@
 from ..base import Base
 from .mixins import Timestamp
-from sqlalchemy import Column, UUID, String, Text, ForeignKey
+from sqlalchemy import Column, UUID, String, ForeignKey, Enum
+from .enums import NotificationStatus
 from uuid import uuid4
 
 class Notification(Base, Timestamp):
@@ -9,6 +10,7 @@ class Notification(Base, Timestamp):
     id = Column(UUID, default=uuid4, primary_key=True)
     message = Column(String)
     user_id = Column(ForeignKey()) # I shall have to come back for this
+    status = Column(Enum(NotificationStatus, name="notification_status"), default=NotificationStatus.UNREAD)
 
     def __repr__(self):
         return self.message
