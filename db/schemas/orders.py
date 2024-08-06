12283ac470104d1eea3e0f9d75ed966c68ec2ad1
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-06 11:34:29 +0300

    Setup the order schemas

--- db/schemas/orders.py
+++ db/schemas/orders.py
@@ -1,5 +1,26 @@
 from pydantic import BaseModel
 from uuid import UUID
+from typing import List
+from decimal import Decimal
+from .menu import Menu
+from models.enums import OrderStatus
 
 class OrderBase(BaseModel):
+    amount: Decimal
+    customer_id: UUID
+    items: List[Menu] = []
+
+class OrderCreate(OrderBase):
     pass
+
+"""
+The update schema will not be implemented since we don't the only attribute to
+be changed is the status of the order
+"""
+
+class Order(OrderBase):
+    id: UUID
+    status: OrderStatus
+
+    class Config:
+        from_attributes = True
