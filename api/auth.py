9835c367f805c7d62c6833a1f6e95e34f98c7785
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-08 13:26:37 +0300

    Format to match PEP8 standards

--- api/auth.py
+++ api/auth.py
@@ -6,13 +6,12 @@ from db.schemas.users import CustomerCreate, CustomerSignin, AdminCreate, AdminS
 from db.crud.users import create_customer, get_customer_by_email, create_admin, get_admin_by_email
 from core.security import create_access_token, verify_password
 
-
 router = APIRouter()
-
 
 """
 This file handles all the endpoints for user account creation and signin
 """
+
 
 # Customer routes
 
@@ -23,17 +22,14 @@ async def create_new_customer(customer: CustomerCreate, db: AsyncSession = Depen
             status_code=status.HTTP_409_CONFLICT,
             detail="Customer with that email already exists"
         )
-    
+
     db_customer = await create_customer(db, customer)
     if not db_customer:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error creating account"
-        )
-    
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error creating account")
+
     return {"message": "Account created successfully"}
 
-    
+
 @router.post("/signin", status_code=status.HTTP_200_OK, response_model=Token)
 async def signin(customer: CustomerSignin, db: AsyncSession = Depends(get_db)):
     db_customer = await get_customer_by_email(db, customer.email)
@@ -54,14 +50,14 @@ async def create_new_admin(admin: AdminCreate, db: AsyncSession = Depends(get_db
             status_code=status.HTTP_409_CONFLICT,
             detail="Admin with that email already exists"
         )
-    
+
     db_admin = await create_admin(db, admin)
     if not db_admin:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Error creating account"
         )
-    
+
     return {"message": "Account created successfully"}
 
 
@@ -73,7 +69,7 @@ async def admin_signin(admin: AdminSignin, db: AsyncSession = Depends(get_db)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials"
         )
-    
+
     # Confirm that the object returned by the coroutine above is compatible with the create_access_token
     access_token = create_access_token(data={"sub": db_admin})
     return {"access_token": access_token, "token_type": "bearer"}
