8e55f581ef647ffaa19ab8da7826d3fc33150adf
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-09 10:57:03 +0300

    Add the user type

--- db/schemas/notifications.py
+++ db/schemas/notifications.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from uuid import UUID
-from db.models.enums import NotificationStatus
+from db.models.enums import NotificationStatus, UserType
 
 
 class NotificationBase(BaseModel):
@@ -8,7 +8,8 @@ class NotificationBase(BaseModel):
 
 
 class NotificationCreate(NotificationBase):
-    user_id: UUID  # Update this later in accordance to the model
+    user_id: UUID
+    user_type: UserType
 
 
 """
