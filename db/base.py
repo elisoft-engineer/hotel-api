81d60a1543fc12d817c3ddcbd5354d5d7d871a00
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-12 08:49:46 +0300

    Restructure the docstring

--- db/base.py
+++ db/base.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from core.config import settings
 
 """
-Creation of the asynchronous database engine happens from this file.
-The Base is the used in the db models as the abstract model.
+Creation of the asynchronous database engine happens from this file. The Base is the used in the db models as the 
+abstract model.
 """
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
