9072efbc1fc364a0c14d5e994d1b7c94ae63a8dc
Elkana Maina <elisoft.engineer@gmail.com>
2024-08-13 20:56:26 +0300

    Initial alembic configuration

---
+++ alembic/env.py
@@ -0,0 +1,48 @@
+from logging.config import fileConfig
+from alembic import context
+import sys
+import os
+import asyncio
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+sys.path.append(BASE_DIR)
+
+config = context.config
+
+from db.base import engine, Base
+from db.models import menu, messages, notifications, orders, users
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online():
+    async with engine.begin() as connection:
+        await connection.run_sync(do_run_migrations)
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
