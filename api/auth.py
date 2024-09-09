6f08b4005e105843c7ff40153c9e5d7e9bf22a03
Elkana Maina <elisoft.engineer@gmail.com>
2024-09-09 19:52:49 +0300

    Correct the access of the users on authentication

--- api/auth.py
+++ api/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from db.session import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.schemas.auth import Token
-from db.schemas.users import CustomerCreate, CustomerSignin, AdminCreate, AdminSignin
+from db.schemas.users import CustomerCreate, CustomerSignin, AdminCreate, AdminSignin, Admin, Customer
 from db.crud.users import create_customer, get_customer_by_email, create_admin, get_admin_by_email
 from core.security import create_access_token, verify_password
 
@@ -17,7 +17,8 @@ This file handles all the endpoints for user account creation and signin
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
 async def create_new_customer(customer: CustomerCreate, db: AsyncSession = Depends(get_db)):
-    if get_customer_by_email(db, customer.email):
+    existing_customer = await get_customer_by_email(db, customer.email)
+    if existing_customer:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="Customer with that email already exists"
@@ -37,15 +38,24 @@ async def signin(customer: CustomerSignin, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
     # Confirm that the object returned by the coroutine above is compatible with the create_access_token
-    access_token = create_access_token(data={"sub": db_customer})
+    access_token = create_access_token(
+        data={"sub": Customer(
+            id=db_customer.id,
+            first_name=db_customer.first_name,
+            last_name=db_customer.last_name,
+            is_active=db_customer.is_active,
+            user_type=db_customer.user_type
+        )}
+    )
     return {"access_token": access_token, "token_type": "bearer"}
 
 
 # Admin routes
 
-@router.post("/signup/admin", status_code=status.HTTP_201_CREATED)
+@router.post("/admin/signup", status_code=status.HTTP_201_CREATED)
 async def create_new_admin(admin: AdminCreate, db: AsyncSession = Depends(get_db)):
-    if get_admin_by_email(db, admin.email):
+    existing_admin = await get_admin_by_email(db, admin.email)
+    if existing_admin:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="Admin with that email already exists"
@@ -61,7 +71,7 @@ async def create_new_admin(admin: AdminCreate, db: AsyncSession = Depends(get_db
     return {"detail": "Account created successfully"}
 
 
-@router.post("/signin/admin", response_model=Token, status_code=status.HTTP_200_OK)
+@router.post("/admin/signin", response_model=Token, status_code=status.HTTP_200_OK)
 async def admin_signin(admin: AdminSignin, db: AsyncSession = Depends(get_db)):
     db_admin = await get_admin_by_email(db, admin.email)
     if not db_admin or not verify_password(admin.password, db_admin.password):
