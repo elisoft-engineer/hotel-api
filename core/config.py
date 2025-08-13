a97093bac3b2c3c38e60d78f40552368175117e4
Elkana Maina <elisoft.engineer@gmail.com>
2025-08-13 13:07:42 +0300

    Use pydantic settings

--- core/config.py
+++ core/config.py
@@ -1,29 +1,29 @@
-from dotenv import load_dotenv
-from os import getenv, path, makedirs
-
-load_dotenv()
-
-"""
-This file handles the app configuration such as the database uri, file system
-management, e.t.c
-"""
+# core/config.py
+from pydantic_settings import BaseSettings
+from os import path, makedirs
 
 
-class Settings:
-    SECRET_KEY: str = getenv("SECRET_KEY")
-    ALGORITHM: str = getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-    DATABASE_URL: str = getenv('DATABASE_URL')
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_LIFETIME: int
+    DATABASE_URL: str
 
-    BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
-    FILES_DIR = path.join(BASE_DIR, "files")
-    MENU_DIR = path.join(FILES_DIR, "menu")
-    MENU_IMAGES_DIR = path.join(MENU_DIR, "images")
-    MENU_THUMBNAILS_DIR = path.join(MENU_DIR, "thumbnails")
+    BASE_DIR: str = path.dirname(path.dirname(path.abspath(__file__)))
+    FILES_DIR: str = path.join(BASE_DIR, "files")
+    MENU_DIR: str = path.join(FILES_DIR, "menu")
+    MENU_IMAGES_DIR: str = path.join(MENU_DIR, "images")
+    MENU_THUMBNAILS_DIR: str = path.join(MENU_DIR, "thumbnails")
 
-    makedirs(FILES_DIR, exist_ok=True)
-    makedirs(MENU_DIR, exist_ok=True)
-    makedirs(MENU_IMAGES_DIR, exist_ok=True)
-    makedirs(MENU_THUMBNAILS_DIR, exist_ok=True)
+    class Config:
+        env_file = ".env.dev"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        makedirs(self.FILES_DIR, exist_ok=True)
+        makedirs(self.MENU_DIR, exist_ok=True)
+        makedirs(self.MENU_IMAGES_DIR, exist_ok=True)
+        makedirs(self.MENU_THUMBNAILS_DIR, exist_ok=True)
+
 
 settings = Settings()
