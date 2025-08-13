ba6e18f8090098ffa6c2276e54c53ff38f2c8897
Elkana Maina <elisoft.engineer@gmail.com>
2025-08-13 13:07:13 +0300

    Use config's settings for database url

--- alembic/env.py
+++ alembic/env.py
@@ -7,11 +7,13 @@ from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+load_dotenv(os.path.join(BASE_DIR, ".env.dev"))
 
 sys.path.append(BASE_DIR)
 
 config = context.config
+
+config.set_main_option("sqlalchemy.url", os.getenv('DATABASE_URL', ''))
 
 from db.base import engine, Base
 from db.models import menu, messages, notifications, orders, users
@@ -20,7 +22,6 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
-
 
 def run_migrations_offline() -> None:
     url = config.get_main_option("sqlalchemy.url")
