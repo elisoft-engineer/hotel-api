07a95e1a70a2395ddfe3592e12ec490144af9e91
Elkana Maina <elisoft.engineer@gmail.com>
2025-08-15 17:13:08 +0300

    Refactor to one model User

--- auth/models.py
+++ auth/models.py
@@ -1,58 +1,21 @@
-from enum import Enum
 from uuid import uuid4
 
-from sqlalchemy import UUID, Boolean, Column, String, text
-from sqlalchemy.dialects import postgresql
-from sqlalchemy.orm import declarative_mixin, relationship
+from sqlalchemy import UUID, Boolean, Column, String
 
 from core.database import Base
 from core.models import Timestamp
 
-"""
-A base user class is more robust and precise as we just have to subclass it to
-manage both the admins and customers.
-"""
 
-class UserType(Enum):
-    ADMIN = "admin"
-    CUSTOMER = "customer"
-
-
-@declarative_mixin
-class User:
-    __abstract__ = True
+class User(Base, Timestamp):
+    __tablename__ = "users"
 
     id = Column(UUID, primary_key=True, default=uuid4)
     email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
-
-
-class Admin(Base, User, Timestamp):
-    __tablename__ = "admins"
-    employee_id = Column(String, index=True)
-    user_type = Column(
-        postgresql.ENUM(*[e.value for e in UserType], name="user_type", create_type=False),
-        default=UserType.ADMIN.value,
-        server_default=text(f"'{UserType.ADMIN.value}'::user_type"),
-        nullable=False
-    )
-
-
-class Customer(Base, User, Timestamp):
-    __tablename__ = "customers"
-
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
-    user_type = user_type = Column(
-        postgresql.ENUM(*[e.value for e in UserType], name="user_type", create_type=False),
-        default=UserType.CUSTOMER.value,
-        server_default=text(f"'{UserType.CUSTOMER.value}'::user_type"),
-        nullable=False
-    )
-
-    reviews = relationship("Review", back_populates="customer")
-    orders = relationship("Order", back_populates="customer")
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    is_staff = Column(Boolean, default=False)
 
     def __repr__(self):
         return f"{self.__class__.__name__}: {self.email}"
