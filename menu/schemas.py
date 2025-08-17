935ce6f641808a29faade066d2feff641a52e4bd
Elkana Maina <elisoft.engineer@gmail.com>
2025-08-17 19:30:55 +0300

    Move review schemas to review module

--- menu/schemas.py
+++ menu/schemas.py
@@ -27,26 +27,3 @@ class Menu(MenuBase):
 
     class Config:
         from_attributes: bool = True
-
-
-class ReviewBase(BaseModel):
-    message: str
-    rating: int
-
-
-class ReviewCreate(ReviewBase):
-    menu_id: UUID
-    customer_id: UUID
-
-
-class ReviewUpdate(ReviewBase):
-    pass
-
-
-class Review(ReviewBase):
-    id: UUID
-    menu_id: UUID
-    customer_id: UUID
-
-    class Config:
-        from_attributes: bool = True
