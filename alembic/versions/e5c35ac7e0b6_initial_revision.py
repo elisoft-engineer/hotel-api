774b321dff99c5b3c2110d934c3606087b7c1638
Elkana Maina <elisoft.engineer@gmail.com>
2025-08-17 20:49:29 +0300

    Regenerate migration

---
+++ alembic/versions/e5c35ac7e0b6_initial_revision.py
@@ -0,0 +1,114 @@
+"""Initial Revision
+
+Revision ID: e5c35ac7e0b6
+Revises: 
+Create Date: 2025-08-17 20:44:40.136142
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision: str = 'e5c35ac7e0b6'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    bind = op.get_bind()
+    postgresql.ENUM('read', 'unread', name='notification_status').create(bind)
+    postgresql.ENUM('main courses', 'drinks', 'other', name='menu_category').create(bind)
+    postgresql.ENUM('pending', 'completed', 'cancelled', name='order_status').create(bind)
+    op.create_table('menu',
+    sa.Column('id', sa.UUID(), nullable=False),
+    sa.Column('name', sa.String(), nullable=False),
+    sa.Column('description', sa.Text(), nullable=False),
+    sa.Column('price', sa.DECIMAL(), nullable=False),
+    sa.Column('category', postgresql.ENUM('main courses', 'drinks', 'other', name='menu_category', create_type=False), server_default=sa.text("'other'::menu_category"), nullable=False),
+    sa.Column('image', sa.String(), nullable=True),
+    sa.Column('thumbnail', sa.String(), nullable=True),
+    sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('modified_at', sa.DateTime(timezone=True), nullable=True),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_menu_name'), 'menu', ['name'], unique=False)
+    op.create_table('messages',
+    sa.Column('id', sa.UUID(), nullable=False),
+    sa.Column('name', sa.String(), nullable=False),
+    sa.Column('email', sa.String(), nullable=False),
+    sa.Column('detail', sa.Text(), nullable=False),
+    sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('modified_at', sa.DateTime(timezone=True), nullable=True),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('notifications',
+    sa.Column('id', sa.UUID(), nullable=False),
+    sa.Column('message', sa.String(), nullable=False),
+    sa.Column('user_id', sa.UUID(), nullable=False),
+    sa.Column('status', postgresql.ENUM('read', 'unread', name='notification_status', create_type=False), server_default=sa.text("'unread'::notification_status"), nullable=False),
+    sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('modified_at', sa.DateTime(timezone=True), nullable=True),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('users',
+    sa.Column('id', sa.UUID(), nullable=False),
+    sa.Column('email', sa.String(), nullable=False),
+    sa.Column('first_name', sa.String(), nullable=True),
+    sa.Column('last_name', sa.String(), nullable=True),
+    sa.Column('password', sa.String(), nullable=False),
+    sa.Column('is_active', sa.Boolean(), nullable=True),
+    sa.Column('is_staff', sa.Boolean(), nullable=True),
+    sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('modified_at', sa.DateTime(timezone=True), nullable=True),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_users_email'), 'users', ['email'], unique=True)
+    op.create_table('orders',
+    sa.Column('id', sa.UUID(), nullable=False),
+    sa.Column('amount', sa.DECIMAL(), nullable=False),
+    sa.Column('status', postgresql.ENUM('pending', 'completed', 'cancelled', name='order_status', create_type=False), server_default=sa.text("'pending'::order_status"), nullable=False),
+    sa.Column('user_id', sa.UUID(), nullable=False),
+    sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('modified_at', sa.DateTime(timezone=True), nullable=True),
+    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('reviews',
+    sa.Column('id', sa.UUID(), nullable=False),
+    sa.Column('message', sa.Text(), nullable=False),
+    sa.Column('rating', sa.Integer(), nullable=False),
+    sa.Column('menu_id', sa.UUID(), nullable=False),
+    sa.Column('user_id', sa.UUID(), nullable=False),
+    sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('modified_at', sa.DateTime(timezone=True), nullable=True),
+    sa.ForeignKeyConstraint(['menu_id'], ['menu.id'], ),
+    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('order_menu',
+    sa.Column('order_id', sa.UUID(), nullable=False),
+    sa.Column('menu_id', sa.UUID(), nullable=False),
+    sa.ForeignKeyConstraint(['menu_id'], ['menu.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['order_id'], ['orders.id'], ondelete='CASCADE'),
+    sa.PrimaryKeyConstraint('order_id', 'menu_id')
+    )
+
+
+def downgrade() -> None:
+    bind = op.get_bind()
+    op.drop_table('order_menu')
+    op.drop_table('reviews')
+    op.drop_table('orders')
+    op.drop_index(op.f('ix_users_email'), table_name='users')
+    op.drop_table('users')
+    op.drop_table('notifications')
+    op.drop_table('messages')
+    op.drop_index(op.f('ix_menu_name'), table_name='menu')
+    op.drop_table('menu')
+    postgresql.ENUM('pending', 'completed', 'cancelled', name='order_status').drop(bind)
+    postgresql.ENUM('main courses', 'drinks', 'other', name='menu_category').drop(bind)
+    postgresql.ENUM('read', 'unread', name='notification_status').drop(bind)
