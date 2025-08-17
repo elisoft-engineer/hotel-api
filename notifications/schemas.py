357f8060300f4434924d8cef1bf5d4b62387c999
Elkana Maina <elisoft.engineer@gmail.com>
2025-08-17 19:31:34 +0300

    Remove comments

--- notifications/schemas.py
+++ notifications/schemas.py
@@ -12,11 +12,6 @@ class NotificationCreate(NotificationBase):
     user_type: str
 
 
-"""
-For the Update schema, we only need to update the status
-"""
-
-
 class Notification(NotificationBase):
     id: UUID
     user_id: UUID
