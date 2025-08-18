23b381f9717a6a559215e67d5a85fccd2581f44e
Elkana Maina <elisoft.engineer@gmail.com>
2025-08-18 12:57:05 +0300

    Change user_id from String to ForeignKey

--- notifications/models.py
+++ notifications/models.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from uuid import uuid4
 
-from sqlalchemy import UUID, Column, String, text
+from sqlalchemy import UUID, Column, String, text, ForeignKey
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import relationship
 
@@ -19,7 +19,7 @@ class Notification(Base, Timestamp):
 
     id = Column(UUID, default=uuid4, primary_key=True)
     message = Column(String, nullable=False)
-    user_id = Column(UUID, nullable=False)
+    user_id = Column(ForeignKey("users.id"), nullable=False)
     status = Column(
         postgresql.ENUM(*[e.value for e in NotificationStatus], name="notification_status", create_type=False),
         default=NotificationStatus.UNREAD.value,
