2b0d2889d326ce142d0ff55aeb243cf6624b1583
Elkana Maina <elisoft.engineer@gmail.com>
2025-08-18 12:31:59 +0300

    Add orders, notifications and reviews relationships

--- auth/models.py
+++ auth/models.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from sqlalchemy import UUID, Boolean, Column, String
+from sqlalchemy.orm import relationship
 
 from core.database import Base
 from core.models import Timestamp
@@ -16,6 +17,10 @@ class User(Base, Timestamp):
     password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     is_staff = Column(Boolean, default=False)
+
+    orders = relationship("Order", back_populates="user")
+    notifications = relationship("Notification", back_populates="user", cascade="all, delete-orphan")
+    reviews = relationship("Review", back_populates="user", cascade="all, delete-orphan")
 
     def __repr__(self):
         return f"{self.__class__.__name__}: {self.email}"
